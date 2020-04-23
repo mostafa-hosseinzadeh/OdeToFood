c2b75b94dc2bea25878eb9294c513ba437e6007d
Mostafa Hosseinzadeh <mhzreg@gmail.com>
2020-04-23 15:28:36 +0430

    TypoCorection

--- OdeToFood.Data/Models/Restaurant.cs
+++ OdeToFood.Data/Models/Restaurant.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace OdeToFood.Data.Models
 {
-    public class Resturant
+    public class Restaurant
     {
         public int Id { get; set; }
         public string Name { get; set; }
