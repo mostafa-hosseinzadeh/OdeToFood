6af25205793396a0797bdf5a3906abf27b232ec9
Mostafa Hosseinzadeh <mhzreg@gmail.com>
2020-04-24 02:30:43 +0430

    Configuring Asp.Net using web.config

---
+++ OdeToFood.Web/Controllers/GreetingsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OdeToFood.Web.Models;
+
+namespace OdeToFood.Web.Controllers
+{
+    public class GreetingsController : Controller
+    {
+        // GET: Greetings
+        public ActionResult Index()
+        {
+            var model = new GreetingsViewModel()
+            {
+                Message = ConfigurationManager.AppSettings["message"]
+            };
+
+            return View(model);
+        }
+    }
+}
